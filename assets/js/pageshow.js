229a61990b00319565ad95de788393b2ef899c0d
GraceLanBM <gracelanblue@gmail.com>
2025-11-27 22:19:21 +0800

    Fix bug: can't show the correct page number
    Bug: can't show the correct page number because the language setting buttons also use 'page' class.
    Fix: get elements from the correct div not all document.

--- assets/js/pageshow.js
+++ assets/js/pageshow.js
@@ -19,9 +19,12 @@ function currentPage(n) {
 function showPages(n) {
     let i;
     let pages = document.getElementsByClassName("myPages");
-    let pageNumbers = document.getElementsByClassName("page");
+    let pageDiv = document.getElementById("pagination");
+    let pageNumbers = pageDiv.getElementsByClassName("page");
     let prevButton = document.getElementById("prev");
     let nextButton = document.getElementById("next");
+ 
+    // change next and prev button status when n is the first or last page
     if (n == pages.length) {
         nextButton.classList.add("disabled");
     } else {
@@ -32,12 +35,16 @@ function showPages(n) {
     } else {
         prevButton.classList.remove("disabled");
     }
+    
+    // hide pages & deactive page numbers buttons
     for (i = 0; i < pages.length; i++) {
         pages[i].style.display = "none";
     }
     for (i = 0; i < pageNumbers.length; i++) {
         pageNumbers[i].classList.remove("active");
     }
+    
+    // show the page & change page number button to active
     pages[pageIndex-1].style.display = "block";
     pageNumbers[pageIndex-1].classList.add("active");
 }
