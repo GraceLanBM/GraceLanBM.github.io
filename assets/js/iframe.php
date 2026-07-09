5423a5837528af6483d2edc5b1fa860aac00d74b
GraceLanBM <46943231+GraceLanBM@users.noreply.github.com>
2026-07-09 15:27:31 +0800

    Add new page and php file

---
+++ assets/js/iframe.php
@@ -0,0 +1,14 @@
+<!--
+Source - https://stackoverflow.com/a/66876917
+Posted by Dale
+Retrieved 2026-07-09, License - CC BY-SA 4.0
+-->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php echo file_get_contents($_REQUEST['link']); ?>
+
+</body>
+</html>
